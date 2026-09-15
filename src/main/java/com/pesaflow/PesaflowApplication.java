37b7d06046453f14d8abd56095220bc5978ab6a8
Karanja Kabugi Francis <kabugifrancis34@gmail.com>
2026-09-15 09:16:21 +0300

    first commmit

---
+++ src/main/java/com/pesaflow/PesaflowApplication.java
@@ -0,0 +1,13 @@
+package com.pesaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesaflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesaflowApplication.class, args);
+	}
+
+}
