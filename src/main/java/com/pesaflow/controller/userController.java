62b9a7ae45255594822cb84dc358ab461f0a8c2f
Karanja Kabugi Francis <kabugifrancis34@gmail.com>
2026-09-16 15:23:18 +0300

    dto

--- src/main/java/com/pesaflow/controller/userController.java
+++ src/main/java/com/pesaflow/controller/userController.java
@@ -1,6 +1,13 @@
 package com.pesaflow.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.pesaflow.dto.CreateUserRequest;
+import jakarta.validation.Valid;
+
 
 @RestController
 public class userController {
@@ -9,4 +16,19 @@ public class userController {
         return "Hello, World!";
     }
 
+    @GetMapping("/users/{id}")
+    public String getUser(@PathVariable Long id)
+         {
+        return "user id:" +id ;
+    }
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the userController!";
+    }
+   @PostMapping("/users")
+    public String createUser(@Valid @RequestBody CreateUserRequest request) {
+
+        return "Name: " + request.getName()
+            + ", Email: " + request.getEmail();
+}
 }
