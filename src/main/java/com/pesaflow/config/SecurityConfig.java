3c79b8b9b30393ca7832c1533d59cd0a9913265a
Karanja Kabugi Francis <kabugifrancis34@gmail.com>
2026-09-21 10:05:54 +0300

    CustomUserdetailsservice addition for security

--- src/main/java/com/pesaflow/config/SecurityConfig.java
+++ src/main/java/com/pesaflow/config/SecurityConfig.java
@@ -1,14 +1,30 @@
 package com.pesaflow.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public PasswordEncoder passWordEncoder(){
+    public PasswordEncoder passWordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
 }
